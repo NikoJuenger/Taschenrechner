69126ee504245425f4710764ef70c9225a1e2bb3
Niko Jünger <niko.juenger@gmx.de>
2018-09-11 14:37:07 +0200

    asd

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
+import requests
+print("Hello World!")
 
 
+def request_git():
+    url = 'https://api.github.com/search/repositories?q=language:python&sort=stars'
+
+    r = requests.get(url)
+
+    # Statuscode 200 == ok
+    print(r.status_code)
+    # Save the response
+    response_dict = r.json()
+    print(response_dict.keys())
+
+    print("Incomplete result? ", response_dict['incomplete_results'])
+    if response_dict['incomplete_results']:
+        print("Total repositorys: ", response_dict['total_count'])
+    else:
+        print("Total repositorys: ", response_dict['total_count'])
+
+
+request_git()
